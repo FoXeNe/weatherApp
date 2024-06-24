3acd4b52235b4f0d1712a760ef390b7c13787f61
FoXeN <121698405+FoXeNe@users.noreply.github.com>
2024-06-25 00:29:09 +0300

    done tryExcept

--- app.py
+++ app.py
@@ -5,49 +5,34 @@ import json
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
-try:
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    load_dotenv()
+load_dotenv()
 
-    def get_weather(a):
-        # водим апи ключ
-        apiKey = os.getenv("api_token")
-        # принимает переменную и кладет ее в city
-        city = a
+def get_weather(city):
+    apiKey = os.getenv("api_token")
 
-        # делаем запрос по этому юрл
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}"
 
-        # ответ с юрл
-        response = requests.get(url).json()
-        pretty_response = json.dumps(response, indent=4)
-        print(pretty_response) 
+    response = requests.get(url).json()
 
-        # беру первый элемент из weather в response
-        weather = response["weather"][0]["main"]
+    weather = response["weather"][0]["main"]
+    temp = round(response["main"]["temp"] - 273)
 
-        # беру инфомацию из response и перевожу ее из кельвина в цельсия отнимая 273
-        temp = round(response["main"]["temp"] - 273)
+    return weather, temp
 
-        return weather, temp
+@app.route('/')
+def index():
+    return render_template("index.html")
 
+@app.route('/', methods=['POST'])
+def submit():
+    city = request.form['inputText']
 
-    @app.route('/')
-    def index():
-        # выводит index.html пользователю
-        return render_template("index.html")
-
-    @app.route('/', methods=['POST'])
-    def submit():
-        # принимает текст который ввел пользователь из index.html
-        city = request.form['inputText']
+    try:
         weather, temp = get_weather(city)
-        # вывожу weather.html задавая переменные которые находятся в weather.html
-        return render_template("weather.html", city = city, weather = weather, temp = temp)
+        return render_template("weather.html", city=city, weather=weather, temp=temp)
+    except Exception as e:
+        return render_template("except.html", error=str(e))
 
-
-    app.run(debug= True)
-except:
-    pass
-
+app.run(debug=True)
